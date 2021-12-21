82e923bbce35ba4c4017e5c201760cf989e1dcdc
jithingp <jithingpanoli18@gmail.com>
2021-12-21 11:21:09 +0530

    fetch api and prev,next buttons

--- src/components/News.js
+++ src/components/News.js
@@ -2,45 +2,48 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles= [
-        {
-          "source": { "id": "bbc-news", "name": "BBC News" },
-          "author": "BBC News",
-          "title": "Jailed for praising the wrong cricket team",
-          "description": "An Indian student's family calls for his release after he was arrested for celebrating Pakistan's victory.",
-          "url": "http://www.bbc.co.uk/news/world-asia-india-59702427",
-          "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/D563/production/_122272645_p0bbv819.jpg",
-          "publishedAt": "2021-12-18T02:37:24.6777437Z",
-          "content": "An Indian student has now spent nearly two months in jail for celebrating Pakistans victory in a cricket match. \r\nThe BBCs Rajini Vaidyanathan went to the northern Indian city of Agra to meet his fam… [+152 chars]"
-        },
-        {
-          "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-          "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-      ]
+    
     constructor(){
         super()
-        console.log("hello i am a constructor from news component");
+        
         this.state={
-            articles:this.articles,
-            loading:false
+            articles:[],
+            loading:false,
+            page:1
         }
+    }
+    async componentDidMount(){
+      let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=2a32e4d1fc7b4347848d8ada3513ca4a&page=1&pageSize=20"
+      let data=await fetch(url)
+      let parsedData=await data.json()
+      console.log(parsedData);
+      this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
+    }
+    handleprevclick=async ()=>{
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=2a32e4d1fc7b4347848d8ada3513ca4a&page=${this.state.page-1}&pageSize=20`
+      let data=await fetch(url)
+      let parsedData=await data.json()
+      console.log(parsedData)
+      this.setState({
+        page:this.state.page-1,
+        articles:parsedData.articles
+      })
+    }
+    handlenextclick=async ()=>{
+      if(this.state.page+1>Math.ceil(this.state.totalResults/20)){
+
+      }
+      else{
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=2a32e4d1fc7b4347848d8ada3513ca4a&page=${this.state.page+1}&pageSize=20`
+      let data=await fetch(url)
+      let parsedData=await data.json()
+      console.log(parsedData)
+      this.setState({
+        page:this.state.page+1,
+        articles:parsedData.articles
+      })
+      }
+
     }
     render() {
         return (
@@ -50,14 +53,15 @@ export class News extends Component {
               <div className="row">
               {this.state.articles.map((element)=>{
                  return <div className="col-md-4" key={element.url}>
-                  <NewsItem  title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageurl={element.urlToImage}
+                  <NewsItem  title={element.title?element.title:""} description={element.description?element.description.slice(0,88):""} imageurl={element.urlToImage}
                   newsUrl={element.url}/>
                   </div>
 
               })}
-                 
-                
-              
+              </div>
+              <div className="container d-flex justify-content-between">
+              <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handleprevclick}> &larr; Previous</button>
+              <button type="button" className="btn btn-dark" onClick={this.handlenextclick}>Next &rarr;</button>
               </div>
                
            
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageurl} className="card-img-top" alt="..."/>
+                    <img src={!imageurl?"https://images.livemint.com/img/2021/12/20/600x338/Tedros_1640015135780_1640015136051.JPG":imageurl} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
+                        <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                        <a rel='noreferrer' href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
